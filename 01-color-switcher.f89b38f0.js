cdb9a0a8ca7d31460b9b3c4647f4d2332d3626dd
eazzyk <113462257+eazzyk@users.noreply.github.com>
2023-04-29 17:48:12 +0000

    Deploying to gh-pages from @ eazzyk/goit-js-hw-09@cf716fdfe8794e7a1d9c13f3a55bd2d20a11777c 🚀

---
+++ 01-color-switcher.f89b38f0.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,a=setInterval((()=>{d.style.background=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)}));let a=null;t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(a)}));
+//# sourceMappingURL=01-color-switcher.f89b38f0.js.map
